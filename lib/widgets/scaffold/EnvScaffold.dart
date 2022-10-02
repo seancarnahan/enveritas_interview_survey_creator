cb4355e1835c07bf80a39aa054030482fe955b2e
Sean Carnahan <fundmaker511@gmail.com>
2022-10-02 13:06:14 -0400

    develop: made safearea background color white for IOS

--- lib/widgets/scaffold/EnvScaffold.dart
+++ lib/widgets/scaffold/EnvScaffold.dart
@@ -31,76 +31,79 @@ class EnvScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     IconData leftActionIcon = getActionIcon();
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 80,
-          elevation: 0,
-          leadingWidth: 0,
-          backgroundColor: BrandedColors.white500,
-          title: Padding(
-            padding: const EdgeInsets.only(left: 12),
-            child: Row(
-              children: [
-                Visibility(
-                  visible: topLeftActionIcon != null,
-                  child: EnvGestureDetector(
-                    onTap: () => NavigationService.navigateBack(context),
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 8),
-                      child: Icon(
-                        leftActionIcon,
-                        size: 24,
-                        color: BrandedColors.primary500
+    return Container(
+      color: BrandedColors.white500,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            toolbarHeight: 80,
+            elevation: 0,
+            leadingWidth: 0,
+            backgroundColor: BrandedColors.white500,
+            title: Padding(
+              padding: const EdgeInsets.only(left: 12),
+              child: Row(
+                children: [
+                  Visibility(
+                    visible: topLeftActionIcon != null,
+                    child: EnvGestureDetector(
+                      onTap: () => NavigationService.navigateBack(context),
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 8),
+                        child: Icon(
+                          leftActionIcon,
+                          size: 24,
+                          color: BrandedColors.primary500
+                        ),
                       ),
-                    ),
-                  )
-                ),
-                const Image(image: AssetImage('assets/Enveritas.png'))
-              ],
-            )
-          ),
-          actions: [
-            Visibility(
-              visible: topRightAction != null,
-              child: SizedBox(
-                height: 32,
-                width: 32,
-                child: Row(
-                  children: [
-                    topRightAction ?? const Icon(Icons.question_mark)
-                  ],
-                ),
+                    )
+                  ),
+                  const Image(image: AssetImage('assets/Enveritas.png'))
+                ],
               )
             ),
-            const SizedBox(width: 24, height: 32)
-          ]
-        ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: SizedBox(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ConstrainedBox(
-                  constraints: const BoxConstraints(
-                    maxWidth: DeskTopMaxWidth,
-                    maxHeight: double.infinity
-                  ),
-                  child: Column(
+            actions: [
+              Visibility(
+                visible: topRightAction != null,
+                child: SizedBox(
+                  height: 32,
+                  width: 32,
+                  child: Row(
                     children: [
-                      const SizedBox(height: 12),
-                      pageContent
+                      topRightAction ?? const Icon(Icons.question_mark)
                     ],
-                  )
+                  ),
                 )
-              ],
-            ),
-          )
-        ),
-      )
+              ),
+              const SizedBox(width: 24, height: 32)
+            ]
+          ),
+          body: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxWidth: DeskTopMaxWidth,
+                      maxHeight: double.infinity
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 12),
+                        pageContent
+                      ],
+                    )
+                  )
+                ],
+              ),
+            )
+          ),
+        )
+      ),
     );
   }
 }
